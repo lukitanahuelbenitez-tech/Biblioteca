4d091bb04d22a137aa3d7c03055730683a4ffd76
lukitanahuelbenitez-tech <lukitanahuelbenitez@gmail.com>
2026-08-24 16:45:45 -0300

    primer libro cargado

---
+++ libros.js
@@ -0,0 +1,13 @@
+const libros = [
+    {
+        id: 1,
+        titulo: "El Principito",
+        autor: "Antoine de Saint-Exupéry",
+        genero: "Ficción",
+        anio: 1943,
+        paginas: 96,
+        rating: 4.8,
+        portada: "img/el principito.png",
+        sinopsis: "Un piloto perdido en el desierto conoce a un pequeño principe que viene de otro planeta..."
+    },
+]
