83c64a8d74711badfc95c1bc5a8a60f2b5f1bc59
mikabatirola434-svg <mikabatirola434@gmail.com>
2026-08-25 16:24:50 -0300

    agregado de libros

--- libros.js
+++ libros.js
@@ -8,8 +8,71 @@ const libros = [
         paginas: 96,
         rating: 4.8,
         portada: "img/el principito.png",
-        sinopsis: "Un piloto varado en el desierto que conoce a un pequeño príncipe de otro planeta. El niño comparte las historias de sus viajes por distintos asteroides, enseñando valiosas lecciones sobre el amor, la amistad y la inocencia que los adultos olvidan."
+        sinopsis: "Un piloto varado en el desierto conoce a un pequeño principe que viene de otro planeta. El niño comparte las historias de sus viajes por distintos asteoroides, enseñando valiosas lecciones sobre el amor, la amistad y la inocencia que los adultos olvidan"
     },
 
-    
+    {
+        id: 2,
+        titulo: "Cien años de soledad",
+        autor: "Gabriel García Márquez",
+        genero: "Ficción épica",
+        anio: 1967,
+        paginas: 747,
+        rating: 4.12,
+        portada: "img/cienaños.png",
+        sinopsis: "Narra la historia de siete generaciones de la familia Buendia en el publo ficticio de Macondo. La novela muestra el auge y la caida de la aldea y de su estirpe, marcados por la soledad, el incesto y las guerras y elementos de realismo mágico"
+    },
+
+    {
+        id: 3,
+        titulo: "Harry Potter y la piedra filosofal",
+        autor: "J.K Rowling",
+        genero: "Fantasia",
+        anio: 1997,
+        paginas: 333,
+        rating: 4.4,
+        portada: "img/harrypotter.png",
+        sinopsis: "Harry Potter ni siquiera había oído hablar de Hogwarts cuando las cartas empezaron a llegar al felpudo del número cuatro de Privet Drive. Dirigidas con tinta verde sobre pergamino amarillento con un sello púrpura, fueron rápidamente confiscadas por sus horribles tíos. Entonces, en el undécimo cumpleaños de Harry, un hombre enorme y de ojos saltones llamado Rubeus Hagrid irrumpió con una noticia asombrosa: Harry Potter era un mago y tenía un lugar en la Escuela Hogwarts de Magia y Hechicería. ¡Una aventura increíble estaba a punto de comenzar!"
+    },
+
+
+    {
+        id: 4,
+        titulo: "1984",
+        autor: "George Orwell",
+        genero: "Ciencia Ficción",
+        anio: 1949,
+        paginas: 368,
+        rating: 4.2,
+        portada: "img/1984.png",
+        sinopsis: " La historia de la pesadilla de un hombre que persigue un amor prohibido a tráves de un mundo gobernado por estados en guerra y una estructura de poder que coontrola no solo la información, si no también el pensamiento y la memoria individuales."
+    },
+
+
+    {
+        id: 5,
+        titulo: "Orgullo y prejuicio",
+        autor: "Jane Austen",
+        genero: "Romance",
+        anio: 1813,
+        paginas: 544,
+        rating: 4.3,
+        portada: "img/orgulloyprejuicio.png",
+        sinopsis: "Orgullo y prejuicio es una novela de amor o, mejor, una novela de enamorados. Bingley y Jane, Darcy y Elisabeth, Lydia y Wickham luchan para obtener el objeto de su pasión, deben jugar el juego que la sociedad en que viven les propone y deben ganarlo. Sin saltarse las reglas, pero con un tesón capaz de vencer cualquier barrera, llegarán a toda costa a ese matrimonio que para ellos habrá de marcar el inicio de la felicidad soñada."
+    },
+
+
+    {
+        id: 6,
+        titulo: "Orgullo y prejuicio",
+        autor: "Jane Austen",
+        genero: "Romance",
+        anio: 1813,
+        paginas: 544,
+        rating: 4.3,
+        portada: "img/orgulloyprejuicio.png",
+        sinopsis: "Orgullo y prejuicio es una novela de amor o, mejor, una novela de enamorados. Bingley y Jane, Darcy y Elisabeth, Lydia y Wickham luchan para obtener el objeto de su pasión, deben jugar el juego que la sociedad en que viven les propone y deben ganarlo. Sin saltarse las reglas, pero con un tesón capaz de vencer cualquier barrera, llegarán a toda costa a ese matrimonio que para ellos habrá de marcar el inicio de la felicidad soñada."
+    },
+
+
 ]
